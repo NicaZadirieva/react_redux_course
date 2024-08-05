037a71299ec6ed8ddfeea096573ca91afbae11e3
Veronica <no.demchenko@yandex.ru>
2024-08-05 23:48:10 +0300

    feat: add Button component
    Add util Button component

--- search-films/CHANGELOG.md
+++ search-films/CHANGELOG.md
@@ -1,3 +1,9 @@
+#### 0.0.1 (2024-08-05)
+
+##### New Features
+
+- add Button component (308b95a2)
+
 #### 0.0.0 (2024-08-05)
 
 ##### Refactors
--- search-films/src/App.jsx
+++ search-films/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './App.css';
+import Button from './components/Button';
 
 function App() {
-  return (
-    <>
-    </>
-  )
+	return (
+		<>
+			<Button text="Искать"/>
+		</>
+	);
 }
 
-export default App
+export default App;
