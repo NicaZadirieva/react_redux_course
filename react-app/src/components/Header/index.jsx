02b53f0adcc396ec4412df94cb9d18bc86603cc8
Veronica <no.demchenko@yandex.ru>
2024-10-28 15:23:00 +0300

    feat(Header): toggle Logo to demonstrate demonstration of virtual DOM

--- react-app/src/components/Header/index.jsx
+++ react-app/src/components/Header/index.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
+import Button from '../Button';
 import SelectUser from '../SelectUser';
 import styles from './styles/index.module.css';
+
+const logos = ['/logo.svg', 'vite.svg'];
+
 export default function Header() {
-	
-	const changeUser = (e) => {
-		console.log(e.target.value); // Выводит выбранный id пользователя в консоль.
+	const [logoIndex, setLogoIndex] = useState(0);
+
+	const toggleLogo = () => {
+		setLogoIndex(state => Number(!state));
 	};
-
 	return (<>
-		<img className={styles.logo} src="/logo.svg" alt="Логотип журнала"/>
-		<SelectUser changeUser={changeUser}/>
-
+		<img className={styles.logo} src={logos[logoIndex]} alt="Логотип журнала"/>
+		<SelectUser />
+		<Button onClick={toggleLogo}>Сменить лого</Button>
 	</>);
 }
