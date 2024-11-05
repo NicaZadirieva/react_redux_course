0fbaba6c05a3f215d1cb4e6bd4f96cb5660b381a
Veronica <no.demchenko@yandex.ru>
2024-11-05 16:00:42 +0300

    fix: исправление боковой панели вместе с лектором

--- pizza-app/src/layout/NavigateLayout/index.tsx
+++ pizza-app/src/layout/NavigateLayout/index.tsx
@@ -1,28 +1,36 @@
 import cn from 'classnames';
 import { Link, Outlet } from 'react-router-dom';
+import Button from '../../components/Button';
 import styles from './index.module.css';
 
 function LinkLayout() {
 	return (
 		<div className={styles['layout']}>
-			<div className={styles['link-layout']}>
+			<div className={styles['sidebar']}>
 				<div className={styles['user']}>
 					<img alt='Аватар' src='/user-avatar.svg' className={styles['user-avatar']}/>
 					<div className={styles['user-name']}>Антон Ларичев</div>
 					<div className={styles['user-email']}>alaricode@ya.ru</div>
 				</div>
-				<div className={styles['link-layout-container']}>
-					<div className={cn(styles['link-container'], styles['menu-link-container'])}>
-						<div className={cn(styles['link-icon'], styles['menu-link-icon'])}></div>
-						<Link to='/'>Меню</Link>
-					</div>
-					<div className={cn(styles['link-container'], styles['cart-link-container'])}>
-						<div className={cn(styles['link-icon'], styles['cart-link-icon'])}></div>
-						<Link to='/cart'>Корзина</Link>
+				<div className={styles['menu']}>
+					<Link to='/' className={cn(styles['link-container'], styles['menu-container'])}>
+						<div className={cn(styles['link-icon'], styles['menu-icon'])}></div>
+						<div>Меню</div>
+					</Link>
+
+					
+					<Link to='/cart' className={cn(styles['link-container'], styles['cart-container'])}>
+						<div className={cn(styles['link-icon'], styles['cart-icon'])}></div>
+						<div>Корзина</div>
 						<div className={styles['goods-icon']}>2</div>
-					</div>
+					</Link>
+
 				
 				</div>
+				<Button className={styles['exit']}>
+					<img alt='Выход' src='/exit-icon.svg' className={styles['exit-icon']}/>
+					Выход
+				</Button>
 			</div>
 			<div className={styles['layout-content']}>
 				<Outlet/>
