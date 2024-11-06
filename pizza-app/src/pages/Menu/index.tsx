82fa52ec8a575992eb6fb34dfc3c56270a90d179
Veronica <no.demchenko@yandex.ru>
2024-11-06 12:44:08 +0300

    fix: пофиксила разметку

--- pizza-app/src/pages/Menu/index.tsx
+++ pizza-app/src/pages/Menu/index.tsx
@@ -9,16 +9,16 @@ function Menu() {
 			<div className={styles['head']}>
 				<Heading>Menu</Heading>
 				<Search placeholder='Введите блюдо или состав'/>
-				<div>
-					<ProductCard
+			</div>
+			<div>
+				<ProductCard
 					    id={1}
-						title='Наслаждение'
-						description='Салями, руккола, помидоры, оливки'
-						rating={4.5}
-						price={300}
-						image={'/product_demo.png'}
-					/>
-				</div>
+					title='Наслаждение'
+					description='Салями, руккола, помидоры, оливки'
+					rating={4.5}
+					price={300}
+					image={'/product_demo.png'}
+				/>
 			</div>
 		</>
 	);
