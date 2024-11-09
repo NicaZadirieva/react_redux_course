0f83869fc7222d2fc81632d6be4b1175e3720983
Veronica <no.demchenko@yandex.ru>
2024-11-09 20:23:09 +0300

    feat: add grid for products

--- pizza-app/src/components/MenuList/index.tsx
+++ pizza-app/src/components/MenuList/index.tsx
@@ -1,16 +1,18 @@
 import ProductCard from '../ProductCard';
 import { MenuListProps } from './MenuListProps';
+import styles from './index.module.css';
 
 export function MenuList({products}: MenuListProps) {
-	return products.map((product) => {
-		return <ProductCard
-			key={product.id}
-			id={product.id}
-			name={product.name}
-			description={product.ingredients.join(', ')}
-			rating={product.rating}
-			price={product.price}
-			image={product.image}
-		/>;
-	})
+	return <div className={styles.wrapper}>
+		{products.map((product) => {
+			return <ProductCard
+				key={product.id}
+				id={product.id}
+				name={product.name}
+				description={product.ingredients.join(', ')}
+				rating={product.rating}
+				price={product.price}
+				image={product.image}
+			/>;}
+		)}</div>;
 }
