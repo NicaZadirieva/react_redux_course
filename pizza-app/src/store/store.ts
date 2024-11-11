a4c53205ab4ba74021695f8fb68d1aa0a62eda76
Veronica <no.demchenko@yandex.ru>
2024-11-11 16:14:53 +0300

    feat: impl of auth worked with redux toolkit

--- pizza-app/src/store/store.ts
+++ pizza-app/src/store/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../store/user.slice';
+import userReducer, { JWT_PERSISTENT_STATE } from '../store/user.slice';
+import { saveState } from './storage';
 
 
 export const store = configureStore({
@@ -9,6 +10,9 @@ export const store = configureStore({
 
 });
 
+store.subscribe(() => {
+	saveState({ jwt: store.getState().user.jwt }, JWT_PERSISTENT_STATE);
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type appDispatch = typeof store.dispatch;
