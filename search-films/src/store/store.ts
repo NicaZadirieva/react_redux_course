72f7ae7d6f45d6ce2c0d8d79447c4b59cb62dd4e
Veronica <no.demchenko@yandex.ru>
2024-11-18 19:28:49 +0300

    chore: delete TODO

--- search-films/src/store/store.ts
+++ search-films/src/store/store.ts
@@ -1,21 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
-import favourSlice, { FILMS_PERSISTENT_STATE } from './films.slice';
-import { saveState } from './storage';
-import userSlice, { USER_PERSISTENT_STATE } from './user.slice';
+import { configureStore } from "@reduxjs/toolkit";
+import favourSlice, { FILMS_PERSISTENT_STATE } from "./films.slice";
+import { saveState } from "./storage";
+import userSlice, { USER_PERSISTENT_STATE } from "./user.slice";
 
 export const store = configureStore({
-	// TODO: для полноценной работы приложения необходимо дореализовать user.slice.ts по аналогии с pizza-app
-	reducer: {
-        favors: favourSlice,
-		users: userSlice
-	}
+  reducer: {
+    favors: favourSlice,
+    users: userSlice,
+  },
 });
 
 store.subscribe(() => {
-	saveState({ favorites: store.getState().favors.favorites}, FILMS_PERSISTENT_STATE);
-	
-	saveState({ data: store.getState().users.data, currentUser: store.getState().users.currentUser},
-	USER_PERSISTENT_STATE);
+  saveState(
+    { favorites: store.getState().favors.favorites },
+    FILMS_PERSISTENT_STATE
+  );
+
+  saveState(
+    {
+      data: store.getState().users.data,
+      currentUser: store.getState().users.currentUser,
+    },
+    USER_PERSISTENT_STATE
+  );
 });
 
 export type RootState = ReturnType<typeof store.getState>;
